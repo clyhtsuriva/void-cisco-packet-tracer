7a4617b81393d1d1924fb72475c78ab453421976
p0txky <65337802+p0txky@users.noreply.github.com>
2020-10-06 11:54:24 +0800

    Update install_pt.sh

--- install_pt.sh
+++ install_pt.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #
-#Baixando e desempacotando o arquivo .deb
-#
+#EXTRACT FILES
+#{{{
 echo ""
-echo "Downloading: PacketTracer_731_amd64.deb..." && curl --progress-bar --remote-name --location "http://cs3.calstatela.edu/~egean/cs4471/software/Cisco%20Packet%20Tracer%207.3.1/PacketTracer_731_amd64.deb"
 DIR="/tmp/PacketTracer/"
 if [ -d "$DIR" ]; then
-	rm -rf "$DIR"
+        rm -rf "$DIR"
 else
-	mkdir /tmp/PacketTracer/
+        mkdir /tmp/PacketTracer/
 fi
 cp PacketTracer_731_amd64.deb /tmp/PacketTracer/
 cd /tmp/PacketTracer/
@@ -18,53 +17,121 @@ tar -C control -Jxf control.tar.xz
 mkdir data
 tar -C data -Jxf data.tar.xz
 cd data
+#}}}
 #
-#Remove a instalação atual do PacketTracer (geralmente instalado em /opt/pt)
-#
+#REMOVE PREVIOUS VERSION
+#{{{
 rm -rf /opt/pt
 rm -rf /usr/share/applications/cisco-pt7.desktop
 rm -rf /usr/share/applications/cisco-ptsa7.desktop
-rm -rf /usr/share/icons/hicolor/48x48/apps/pt7.png
+rm -rf /usr/share/icons/gnome/48x48/mimetypes/pka.png
+rm -rf /usr/share/icons/gnome/48x48/mimetypes/pkt.png
+rm -rf /usr/share/icons/gnome/48x48/mimetypes/pkz.png
+rm -rf /usr/share/mime/packages/cisco-pka.xml
+rm -rf /usr/share/mime/packages/cisco-pkt.xml
+rm -rf /usr/share/mime/packages/cisco-pkz.xml
+#}}}
 #
-#Copia arquivos do PacketTracer
-#
+#COPY FILES USR,OPT
+#{{{
 cp -r usr /
 cp -r opt /
+#}}}
 #
-#Link simbólico para uma biblioteca necessária
-#
-ln -s /usr/lib64/libdouble-conversion.so.3.1.5 /usr/lib64/libdouble-conversion.so.1
-#
-#Atualiza o ícone e a associação de arquivos
-#
+#Setup The Desktop
+#{{{
 xdg-desktop-menu install /usr/share/applications/cisco-pt7.desktop
 xdg-desktop-menu install /usr/share/applications/cisco-ptsa7.desktop
 update-mime-database /usr/share/mime
 gtk-update-icon-cache --force --ignore-theme-index /usr/share/icons/gnome
 xdg-mime default cisco-ptsa7.desktop x-scheme-handler/pttp
+#}}}
 #
-#Link simbólico para PacketTracer
+#Remove temporary files
+#{{{
+rm -rf /tmp/PacketTracer/
+#}}}
+#!/bin/bash
 #
-ln -sf /opt/pt/packettracer /usr/local/bin/packettracer
-#
-#Configura variáveis de ambiente
-#
-cat << "EOF">> /etc/profile.local
-PT7HOME=/opt/pt
-export PT7HOME
-QT_DEVICE_PIXEL_RATIO=auto
-export QT_DEVICE_PIXEL_RATIO
-EOF
-#
-#Remove arquivos utilizados durante a instalação
-#
-rm -rf /tmp/PacketTracerInst/
-#
-#Sai do script
+#EXTRACT FILES
+#{{{
 echo ""
-echo "Aperte <ENTER> para continuar..."
+DIR="/tmp/PacketTracer/"
+if [ -d "$DIR" ]; then
+        rm -rf "$DIR"
+else
+        mkdir /tmp/PacketTracer/
+fi
+cp PacketTracer_731_amd64.deb /tmp/PacketTracer/
+cd /tmp/PacketTracer/
+ar -xv PacketTracer_731_amd64.deb
+mkdir control
+tar -C control -Jxf control.tar.xz
+mkdir data
+tar -C data -Jxf data.tar.xz
+cd data
+#}}}
+#
+#REMOVE PREVIOUS VERSION
+#{{{
+rm -rf /opt/pt
+rm -rf /usr/share/applications/cisco-pt7.desktop
+rm -rf /usr/share/applications/cisco-ptsa7.desktop
+rm -rf /usr/share/icons/gnome/48x48/mimetypes/pka.png
+rm -rf /usr/share/icons/gnome/48x48/mimetypes/pkt.png
+rm -rf /usr/share/icons/gnome/48x48/mimetypes/pkz.png
+rm -rf /usr/share/mime/packages/cisco-pka.xml
+rm -rf /usr/share/mime/packages/cisco-pkt.xml
+rm -rf /usr/share/mime/packages/cisco-pkz.xml
+#}}}
+#
+#COPY FILES USR,OPT
+#{{{
+cp -r usr /##
+#It's done
+#{{{
+echo ""
+echo "Press <ENTER> to continue..."
 echo ""
 read 
 echo "Bye!Bye!"
 echo ""
 echo ""
+#}}}
+#It's done
+#{{{
+echo ""
+echo "Press <ENTER> to continue..."
+echo ""
+read 
+echo "Bye!Bye!"
+echo ""
+echo ""
+#}}}
+cp -r opt /
+#}}}
+#
+#Setup The Desktop
+#{{{
+xdg-desktop-menu install /usr/share/applications/cisco-pt7.desktop
+xdg-desktop-menu install /usr/share/applications/cisco-ptsa7.desktop
+update-mime-database /usr/share/mime
+gtk-update-icon-cache --force --ignore-theme-index /usr/share/icons/gnome
+xdg-mime default cisco-ptsa7.desktop x-scheme-handler/pttp
+#}}}
+#
+#Remove temporary files
+#{{{
+rm -rf /tmp/PacketTracer/
+#}}}
+#
+#It's done
+#{{{
+echo ""
+echo "Press <ENTER> to continue..."
+echo ""
+read 
+echo "Bye!Bye!"
+echo ""
+echo ""
+#}}}
