a30800a4a2894bd540ca44e1ad24019d583e7753
p0txky <1000100ten@gmail.com>
2020-11-18 09:21:18 +0800

    uninstall script added!

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+rm -rf /opt/pt
+rm -rf /usr/share/applications/cisco-pt7.desktop
+rm -rf /usr/share/applications/cisco-ptsa7.desktop
+rm -rf /usr/share/icons/gnome/48x48/mimetypes/pka.png
+rm -rf /usr/share/icons/gnome/48x48/mimetypes/pkt.png
+rm -rf /usr/share/icons/gnome/48x48/mimetypes/pkz.png
+rm -rf /usr/share/mime/packages/cisco-pka.xml
+rm -rf /usr/share/mime/packages/cisco-pkt.xml
+rm -rf /usr/share/mime/packages/cisco-pkz.xml
